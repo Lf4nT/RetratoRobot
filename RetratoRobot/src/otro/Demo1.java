ca993cb18b202d58a4a8cf5fa233da43238afb69
Javier Moran <javiermoran7@gmail.com>
2019-01-22 09:28:27 +0100

    OBVAIMDEBEST

--- RetratoRobot/src/otro/Demo1.java
+++ RetratoRobot/src/otro/Demo1.java
@@ -1,13 +1,20 @@
 package otro;
 
 import java.util.Scanner;
+import otro2.RetratoRobot;
 
-public class otro2 {
+public class Demo1 {
 
 	static Scanner in = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.println(new RetratoRobot());
+		int pelo = elegirRasgo(RetratoRobot.getRasgos(0));
+		int ojos = elegirRasgo(RetratoRobot.getRasgos(1));
+		int nariz = elegirRasgo(RetratoRobot.getRasgos(2));
+		int boca = elegirRasgo(RetratoRobot.getRasgos(3));
+		int barbilla = elegirRasgo(RetratoRobot.getRasgos(4));
+		RetratoRobot retrato = new RetratoRobot(pelo, ojos, nariz, boca, barbilla);
+		System.out.println(retrato);
 	}
 
 	public static int elegirRasgo(String[] rasgos) {
---
+++ RetratoRobot/src/otro/Demo2.java
@@ -0,0 +1,14 @@
+package otro;
+
+import java.util.Scanner;
+import otro2.RetratoRobot;
+
+public class Demo2 {
+
+	static Scanner in = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println(new RetratoRobot());
+	}
+
+}
